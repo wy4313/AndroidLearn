c20de53c86b5e9a7de1fbe1046be8c124fd30558
wy4313 <wy4313@163.com>
2017-01-25 16:05:47 +0800

    commit an android program include 3 module

---
+++ learn/learn01/src/main/java/com/example/learn/learn01/MainActivityLearn01Fragment.java
@@ -0,0 +1,22 @@
+package com.example.learn.learn01;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityLearn01Fragment extends Fragment {
+
+    public MainActivityLearn01Fragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_activity_learn01, container, false);
+    }
+}
